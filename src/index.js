23c116bb713ebddbe52ad29e1abd728eedb0e461
quyena357 <quyena357@gmail.com>
2023-01-10 11:30:34 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const route = require('./routes')
+
+const app = express()
+const port = 3000
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Middleware for POST method
+app.use(express.urlencoded(
+  {
+    extended: true
+  }
+))
+app.use(express.json())
+
+// HTTP Logger
+// app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', handlebars.engine({extname: '.hbs'}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname,'resource/views'))
+
+// Routes init
+route(app)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
